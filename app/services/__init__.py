45c6189f19f146f7a4ac960d12c59dd4857a66ab
Jithin Mathews <jithinmathws@gmail.com>
2026-06-01 20:11:02 +0530

    Fix services package initialization

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,4 +1,1 @@
-def __init__(self, db: Session):
-    self.db = db
-    self.weights = DEFAULT_SCREENING_WEIGHTS
-    self.material_risk_service = MaterialRiskService(db)
+"""Service layer package."""
